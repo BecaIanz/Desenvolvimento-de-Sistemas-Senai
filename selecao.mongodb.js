dd3cf7cec9cd9f94b8edcb80719e66f95b1eb393
BecaIanz <silveiraianzr@gmail.com>
2026-02-03 09:22:53 -0300

    Find com mais de um parametro

--- selecao.mongodb.js
+++ selecao.mongodb.js
@@ -1,3 +1,7 @@
 use("Bosch")
 
-db.people.find({ name: /^R/})
+db.people.find({ name: "Helo"})
+
+db.people.find({ name: /^R.*a$/})
+
+db.people.find({ $and: [{ name: "Thaís"}, { lastName : "Schitz"}]})
