14473f17f8b86e2d840d1b43287d6833a66cedc9
BecaIanz <silveiraianzr@gmail.com>
2026-02-03 10:19:35 -0300

    Find com comparação de maior

--- selecao.mongodb.js
+++ selecao.mongodb.js
@@ -5,3 +5,7 @@ db.people.find({ name: "Helo"})
 db.people.find({ name: /^R.*a$/})
 
 db.people.find({ $and: [{ name: "Thaís"}, { lastName : "Schitz"}]})
+
+db.people.find({ salary: {$gte: 2200}})
+
+db.people.find({salary: { $gte: 2200}}, {name: 1, lastName: 1})
