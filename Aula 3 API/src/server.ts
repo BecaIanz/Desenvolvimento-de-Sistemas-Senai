356c25c14a4e2d6891baf3b3247a3187a7c7735c
BecaIanz <silveiraianzr@gmail.com>
2026-02-09 10:54:07 -0300

    Muitas coisas de api

--- Aula 3 API/src/server.ts
+++ Aula 3 API/src/server.ts
@@ -1,7 +1,14 @@
-import express from "express"
+import express, { response } from "express"
+import routes from "./routes/routes.ts"
 
 const port = 8080
 const app = express()
+
+routes(app)
+
+app.get('/', (req, res) => {
+    res.status(200).send({response : "Deu tudo certo meu chapa"})
+})
 
 app.listen(port, () => {
     console.log(`Serviodor rodando na porta ${port}`)
