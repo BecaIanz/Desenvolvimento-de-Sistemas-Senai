fe231a1f78ba9abce836c851aa4ab563ef06d876
BecaIanz <silveiraianzr@gmail.com>
2026-02-09 10:31:51 -0300

    sim

--- Aula 3 API/src/server.ts
+++ Aula 3 API/src/server.ts
@@ -3,27 +3,6 @@ import express from "express"
 const port = 8080
 const app = express()
 
-const pessoas = [
-    {
-        nome: "Rebecaa"
-    },
-    {
-        nome: "Helo"
-    },
-    {
-        nome: "Thaís"
-    }
-]
-
-app.get('/',(req, res) => {
-    res.send({response: "API funcionando"})
-})
-
-app.get('/pessoa', (req, res) => {
-    res.status(200).send({pessoas: pessoas})
-})
-
-
 app.listen(port, () => {
     console.log(`Serviodor rodando na porta ${port}`)
 })
