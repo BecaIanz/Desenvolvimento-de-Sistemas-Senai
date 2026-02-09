aad2b44ed84f0251167a4ab5b378c4e895161a37
BecaIanz <silveiraianzr@gmail.com>
2026-02-09 11:39:32 -0300

    muitos gets e posts

--- Aula 3 API/src/routes/person.ts
+++ Aula 3 API/src/routes/person.ts
@@ -3,4 +3,14 @@ import express, {Request, Response, Router } from 'express'
 const router: Router = express.Router()
 const people: object[]= []
 
+router
+    .post('/cadastro', (req: Request, res: Response) => {
+        const {nome, sobrenome, idade} = req.body
+        people.push({nome, sobrenome, idade})
+        res.status(200).send({message: `Usuário ${nome} cadastrado com sucesso`})
+    })
+    .get('/usuarios', (req: Request, res: Response) => {
+        res.status(200).send({users : people})
+    })
+
 export default router
