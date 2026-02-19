ba46af540222ec7557a2ce8b96e0871c70f0f8e2
BecaIanz <silveiraianzr@gmail.com>
2026-02-19 11:39:25 -0300

    put e delete

--- Aula 3 API/src/routes/person.ts
+++ Aula 3 API/src/routes/person.ts
@@ -18,5 +18,16 @@ router
 
         res.status(200).send({response: people[convertId]})
     })
+    .put("/atualizar/:id",  (req:Request, res:Response) => {
+        const { id } = req.params
+        const { nome, idade } = req.body
+        res.status(200).send({response: `Atualizando usuario ${id} -> ${nome} - ${idade}`})
+
+    })
+    .delete("/deletar/:id", (req:Request, res:Response) => {
+        const { id } = req.params
+        res.status(200).send({response: `Deletando usuario ${id}`})
+
+    })
 
 export default router
